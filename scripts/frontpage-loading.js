42109447034a3cbe04c6060b976e4bc9f97e01d8
Austin philip <austin.bijumon@gmail.com>
2025-02-11 23:34:20 -0800

    Added loading screen

---
+++ scripts/frontpage-loading.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const loadingText = document.getElementsByClassName("centerLoading");
+    let dots = 0; 
+    let repetes = 0;
+
+    setInterval(()=>{
+        dots = (dots + 1) % 4;
+        console.log(loadingText);
+        loadingText[0].innerHTML = `<p>Loading${".".repeat(dots)}</p>`;
+
+        repetes++;
+
+        // if(repetes == 5){
+        //     window.location.href = "./home.html"
+        // }
+    }, 500);
+});
