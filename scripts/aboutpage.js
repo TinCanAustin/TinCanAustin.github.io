1fbc5a76865d8863c9c7fa17fc3c78127db1059c
Austin philip <austin.bijumon@gmail.com>
2025-02-13 08:59:43 -0800

    Fix click to scroll

--- scripts/aboutpage.js
+++ scripts/aboutpage.js
@@ -6,4 +6,24 @@ document.addEventListener("DOMContentLoaded", ()=>{
         const clone = img.cloneNode(true);
         imagesContainer.appendChild(clone);
     });
+
+    const navigators = document.querySelectorAll(".contentFooter");
+    navigators.forEach(button => {
+        let a = button.querySelector("a");
+        if(a.classList.contains("down")){
+            a.addEventListener("click", ()=>{
+                const section = button.parentElement;
+                const next = section.nextElementSibling;
+
+                if(next){
+                    next.scrollIntoView({behavior: 'smooth'});
+                }
+            });
+        }else if(a.classList.contains("up")){
+            a.addEventListener("click", ()=>{
+                const section = document.querySelector("#introContent");
+                section.scrollIntoView({behavior: 'smooth'});
+            });
+        }
+    });
 });
